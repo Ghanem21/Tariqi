d8bb3d61053cb985b42911c934d95bf76c70f051
7odamagdy <hodamagdyf@gmail.com>
2021-09-26 11:46:04 +0200

    1

--- app/src/main/java/com/example/tariqi/splash_screen.java
+++ app/src/main/java/com/example/tariqi/splash_screen.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 
-public class SignInActivity extends AppCompatActivity {
+public class splash_screen extends AppCompatActivity {
 
+    Handler h = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-
+        setContentView(R.layout.activity_splash_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-    }
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splash_screen.this, SignInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
 
+    }
 }
