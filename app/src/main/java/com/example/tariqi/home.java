a8c27304ff500e2220c6f1548f32344fe6a4e7dd
Ghanem21 <ghanem.ana.abdo@gmail.com>
2021-09-26 12:05:20 +0200

    home with navigation drawer

--- app/src/main/java/com/example/tariqi/home.java
+++ app/src/main/java/com/example/tariqi/home.java
@@ -9,10 +9,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -21,11 +25,17 @@ public class home extends AppCompatActivity {
     private ActionBarDrawerToggle drawerToggle;
     private ListView listView;
     private ArrayAdapter adapter;
+    private ImageButton add;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        add = (ImageButton) findViewById(R.id.home_add_btn);
+        //custm toolbar to make drawer over toolbar
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.new_toolbar);
+        setSupportActionBar(toolbar);
         //navigation slideable toggle button
         drawerLayout = findViewById(R.id.drawer);
         drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
@@ -33,30 +43,29 @@ public class home extends AppCompatActivity {
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        /*//create normal Listview
+
+        //create normal Listview
         listView = findViewById(R.id.home_listview);
         ArrayList arrayList = new ArrayList();
         for (int i = 1;i <= 10;i++)
             arrayList.add(i+"");
         adapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item, arrayList);
-        listView.setAdapter(adapter);*/
+        listView.setAdapter(adapter);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"it work",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.add_button,menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.menu_add_btn)
-        {
-            Toast.makeText(getApplicationContext(),"it work",Toast.LENGTH_SHORT).show();
-        }
-        if(drawerToggle.onOptionsItemSelected(item))
-            return true;
+       if(drawerToggle.onOptionsItemSelected(item))
+           return true;
         return true;
     }
 }
