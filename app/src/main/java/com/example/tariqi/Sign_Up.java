407164810ad3771de74ec3981693631d12ff695b
rahma magdy <rahma.magdu909@gmail.com>
2021-09-26 18:23:56 +0200

    history done

--- app/src/main/java/com/example/tariqi/Sign_Up.java
+++ app/src/main/java/com/example/tariqi/Sign_Up.java
@@ -8,11 +8,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class Sign_Up extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Sign_Up.this,ItemHistory.class);
+                startActivity(intent);           }
+        });
     }
 }
