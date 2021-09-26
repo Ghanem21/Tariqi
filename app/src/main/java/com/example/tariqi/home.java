0716889842f29fbd20ca8053b3c1c512821fd92e
Ghanem21 <ghanem.ana.abdo@gmail.com>
2021-09-26 09:02:23 +0200

    home with navigation_menu.xml without dimensions

--- app/src/main/java/com/example/tariqi/home.java
+++ app/src/main/java/com/example/tariqi/home.java
@@ -1,20 +1,45 @@
 package com.example.tariqi;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class home extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class home extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private ListView listView;
+    private ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //navigation slideable toggle button
+        drawerLayout = findViewById(R.id.drawer);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*//create normal Listview
+        listView = findViewById(R.id.home_listview);
+        ArrayList arrayList = new ArrayList();
+        for (int i = 1;i <= 10;i++)
+            arrayList.add(i+"");
+        adapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item, arrayList);
+        listView.setAdapter(adapter);*/
     }
 
     @Override
@@ -30,6 +55,8 @@ public class home extends AppCompatActivity {
         {
             Toast.makeText(getApplicationContext(),"it work",Toast.LENGTH_SHORT).show();
         }
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
         return true;
     }
 }
