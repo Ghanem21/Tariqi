b51f9bfbc1d403e9dbfe7a39b521e9022c3cb751
Ghanem21 <ghanem.ana.abdo@gmail.com>
2021-10-01 17:22:39 +0200

    sign in textview

--- app/src/main/java/com/example/tariqi/Sign_Up.java
+++ app/src/main/java/com/example/tariqi/Sign_Up.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Sign_Up extends AppCompatActivity {
-
+    TextView signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        signIn = findViewById(R.id.tv_sign_in);
+        /*signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Sign_in.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
